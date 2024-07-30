4db849025707135c358f75790517f318cd57a2b2
Christopher Sams <csams@redhat.com>
2024-07-30 15:15:44 -0500

    Add a bit of logging to storage subsystem
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,12 +3,13 @@ package storage
 import (
 	"fmt"
 
+	"github.com/go-kratos/kratos/v2/log"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-func New(c CompletedConfig) (*gorm.DB, error) {
+func New(c CompletedConfig, logger *log.Helper) (*gorm.DB, error) {
 	var opener func(string) gorm.Dialector
 
 	switch c.Database {
@@ -20,5 +21,11 @@ func New(c CompletedConfig) (*gorm.DB, error) {
 		return nil, fmt.Errorf("unrecognized database type: %s", c.Database)
 	}
 
-	return gorm.Open(opener(c.DSN), &gorm.Config{})
+	logger.Infof("Using backing storage: %s", c.Database)
+	db, err := gorm.Open(opener(c.DSN), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("Error opening database: %s", err.Error())
+	}
+
+	return db, nil
 }

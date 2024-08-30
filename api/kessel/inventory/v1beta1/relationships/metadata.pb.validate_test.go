d139bbcc8cfba6c10d3b147ba4d5ac2daca391cb
Randy George <randy.george@redhat.com>
2024-08-30 13:29:19 -0500

    change to include metadata in resource-relationships

---
+++ api/kessel/inventory/v1beta1/relationships/metadata.pb.validate_test.go
@@ -0,0 +1,25 @@
+package relationships
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMetadataValid(t *testing.T) {
+	meta := Metadata{}
+	err := meta.ValidateAll()
+
+	assert.NoError(t, err)
+}
+
+func TestMetadataInvalidLabels(t *testing.T) {
+	meta := Metadata{
+		Labels: []*ResourceLabel{
+			{},
+		},
+	}
+
+	err := meta.ValidateAll()
+	assert.ErrorContains(t, err, "invalid Metadata.Labels[0]")
+}

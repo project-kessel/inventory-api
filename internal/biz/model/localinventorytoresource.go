f2992365f9a885b88fa0318fdea736877300cb4f
Josejulio Martínez <jmartine@redhat.com>
2024-10-10 07:13:41 -0600

    Remove FK on local resource id to resource id table

--- internal/biz/model/localinventorytoresource.go
+++ internal/biz/model/localinventorytoresource.go
@@ -3,7 +3,6 @@ package model
 type LocalInventoryToResource struct {
 	// Our local resource id
 	ResourceId uint64 `gorm:"primarykey"`
-	Resource   Resource
 
 	// Id in reporter's side
 	LocalResourceId string `gorm:"primarykey"`

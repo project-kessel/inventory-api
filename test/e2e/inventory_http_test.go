fc17933d1854ae39675cb9bec5082445f9f6617c
Adam0Brien <aobrien@redhat.com>
2024-11-15 10:48:07 +0000

    combine k8clster endpoints

--- test/e2e/inventory_http_test.go
+++ test/e2e/inventory_http_test.go
@@ -5,17 +5,16 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	nethttp "net/http"
-	"os"
-	"strconv"
-	"testing"
-
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/go-kratos/kratos/v2/transport/http"
 	v1 "github.com/project-kessel/inventory-api/api/kessel/inventory/v1"
 	"github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta1/resources"
 	"github.com/project-kessel/inventory-client-go/v1beta1"
 	"github.com/stretchr/testify/assert"
+	nethttp "net/http"
+	"os"
+	"strconv"
+	"testing"
 )
 
 var inventoryapi_http_url string
@@ -29,9 +28,7 @@ func TestMain(m *testing.M) {
 		log.Error(err)
 		inventoryapi_http_url = "localhost:8081"
 	}
-
 	insecure = true
-
 	insecureTLSstr := os.Getenv("INV_TLS_INSECURE")
 	if insecureTLSstr != "" {
 		var err error
@@ -40,7 +37,6 @@ func TestMain(m *testing.M) {
 			log.Errorf("faild to parse bool INV_TLS_INSECURE %s", err)
 		}
 	}
-
 	certFile := os.Getenv("INV_TLS_CERT_FILE")
 	keyFile := os.Getenv("INV_TLS_KEY_FILE")
 	caFile := os.Getenv("INV_TLS_CA_FILE")
@@ -50,18 +46,15 @@ func TestMain(m *testing.M) {
 		if err != nil {
 			log.Errorf("failed to load client certificate: %v", err)
 		}
-
 		// Load CA cert
 		caCert, err := os.ReadFile(caFile)
 		if err != nil {
 			log.Errorf("failed to read CA certificate: %v", err)
 		}
-
 		caCertPool := x509.NewCertPool()
 		if !caCertPool.AppendCertsFromPEM(caCert) {
 			log.Errorf("failed to append CA certificate")
 		}
-
 		tlsConfig = &tls.Config{
 			Certificates: []tls.Certificate{cert},
 			RootCAs:      caCertPool,
@@ -70,11 +63,9 @@ func TestMain(m *testing.M) {
 		insecure = true
 		log.Info("TLS environment variables not set")
 	}
-
 	result := m.Run()
 	os.Exit(result)
 }
-
 func TestInventoryAPIHTTP_Livez(t *testing.T) {
 	t.Parallel()
 	httpClient, err := http.NewClient(
@@ -84,20 +75,15 @@ func TestInventoryAPIHTTP_Livez(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to create HTTP client: ", err)
 	}
-
 	healthClient := v1.NewKesselInventoryHealthServiceHTTPClient(httpClient)
 	resp, err := healthClient.GetLivez(context.Background(), &v1.GetLivezRequest{})
-
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
-
 	expectedStatus := "OK"
 	expectedCode := uint32(200)
-
 	assert.Equal(t, expectedStatus, resp.Status)
 	assert.Equal(t, expectedCode, resp.Code)
 }
-
 func TestInventoryAPIHTTP_Readyz(t *testing.T) {
 	t.Parallel()
 	httpClient, err := http.NewClient(
@@ -107,33 +93,25 @@ func TestInventoryAPIHTTP_Readyz(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to create HTTP client: ", err)
 	}
-
 	healthClient := v1.NewKesselInventoryHealthServiceHTTPClient(httpClient)
 	resp, err := healthClient.GetReadyz(context.Background(), &v1.GetReadyzRequest{})
-
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
-
 	expectedStatus := "Storage type postgres"
 	expectedCode := uint32(200)
-
 	assert.Equal(t, expectedStatus, resp.Status)
 	assert.Equal(t, expectedCode, resp.Code)
 }
-
 func TestInventoryAPIHTTP_Metrics(t *testing.T) {
 	resp, err := nethttp.Get("http://" + inventoryapi_http_url + "/metrics")
 	if err != nil {
 		t.Fatal("Failed to send request: ", err)
 	}
 	defer resp.Body.Close()
-
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
-
 	expectedStatusCode := 200
 	expectedStatusString := "200 OK"
-
 	assert.Equal(t, expectedStatusCode, resp.StatusCode)
 	assert.Equal(t, expectedStatusString, resp.Status)
 }
@@ -154,58 +132,58 @@ func TestInventoryAPIHTTP_RHELHostLifecycle(t *testing.T) {
 		RhelHost: &resources.RhelHost{
 			Metadata: &resources.Metadata{
 				ResourceType: "rhel_host",
-				WorkspaceId:  "workspace0",
+				WorkspaceId:  "workspace",
 				OrgId:        "",
 			},
 			ReporterData: &resources.ReporterData{
-				ReporterInstanceId: "user@email.com",
+				ReporterInstanceId: "user@example.com",
 				ReporterType:       resources.ReporterData_OCM,
-				ConsoleHref:        "www.web.com",
-				ApiHref:            "www.wb.com",
+				ConsoleHref:        "www.example.com",
+				ApiHref:            "www.example.com",
 				LocalResourceId:    "0123",
-				ReporterVersion:    "0.2",
+				ReporterVersion:    "0.1",
 			},
 		},
 	}
 	opts := getCallOptions()
 	_, err = client.RhelHostServiceClient.CreateRhelHost(context.Background(), &createRequest, opts...)
-	assert.NoError(t, err)
+	assert.NoError(t, err, "Failed to create RhelHost")
 
 	updateRequest := resources.UpdateRhelHostRequest{
 		RhelHost: &resources.RhelHost{
 			Metadata: &resources.Metadata{
 				ResourceType: "rhel_host",
-				WorkspaceId:  "workspace0",
+				WorkspaceId:  "workspace",
 				OrgId:        "",
 			},
 			ReporterData: &resources.ReporterData{
-				ReporterInstanceId: "user@email.com",
+				ReporterInstanceId: "user@example.com",
 				ReporterType:       resources.ReporterData_OCM,
-				ConsoleHref:        "www.web.com",
-				ApiHref:            "www.wb.com",
+				ConsoleHref:        "www.example.com",
+				ApiHref:            "www.example.com",
 				LocalResourceId:    "0123",
-				ReporterVersion:    "0.2",
+				ReporterVersion:    "0.1",
 			},
 		},
 	}
 	_, err = client.RhelHostServiceClient.UpdateRhelHost(context.Background(), &updateRequest, opts...)
-	assert.NoError(t, err)
+	assert.NoError(t, err, "Failed to update RhelHost")
 
 	deleteRequest := resources.DeleteRhelHostRequest{
 		ReporterData: &resources.ReporterData{
-			ReporterInstanceId: "user@email.com",
+			ReporterInstanceId: "user@example.com",
 			ReporterType:       resources.ReporterData_OCM,
-			ConsoleHref:        "www.web.com",
-			ApiHref:            "www.wb.com",
+			ConsoleHref:        "www.example.com",
+			ApiHref:            "www.example.com",
 			LocalResourceId:    "0123",
-			ReporterVersion:    "0.2",
+			ReporterVersion:    "0.1",
 		},
 	}
 	_, err = client.RhelHostServiceClient.DeleteRhelHost(context.Background(), &deleteRequest, opts...)
-	assert.NoError(t, err)
+	assert.NoError(t, err, "Failed to delete RhelHost")
 }
 
-func TestInventoryAPIHTTP_K8SCluster_CreateK8SCluster(t *testing.T) {
+func TestInventoryAPIHTTP_K8SClusterLifecycle(t *testing.T) {
 	t.Parallel()
 	c := v1beta1.NewConfig(
 		v1beta1.WithHTTPUrl(inventoryapi_http_url),
@@ -220,60 +198,6 @@ func TestInventoryAPIHTTP_K8SCluster_CreateK8SCluster(t *testing.T) {
 		K8SCluster: &resources.K8SCluster{
 			Metadata: &resources.Metadata{
 				ResourceType: "k8s_cluster",
-				WorkspaceId:  "",
-				OrgId:        "",
-			},
-			ResourceData: &resources.K8SClusterDetail{
-				ExternalClusterId: "01234",
-				ClusterStatus:     resources.K8SClusterDetail_READY,
-				KubeVersion:       "1.31",
-				KubeVendor:        resources.K8SClusterDetail_OPENSHIFT,
-				VendorVersion:     "4.16",
-				CloudPlatform:     resources.K8SClusterDetail_AWS_UPI,
-				Nodes: []*resources.K8SClusterDetailNodesInner{
-					{
-						Name:   "www.web.com",
-						Cpu:    "7500m",
-						Memory: "30973224Ki",
-						Labels: []*resources.ResourceLabel{
-							{
-								Key:   "has_monster_gpu",
-								Value: "no",
-							},
-						},
-					},
-				},
-			},
-			ReporterData: &resources.ReporterData{
-				ReporterInstanceId: "user@example.com",
-				ReporterType:       resources.ReporterData_ACM,
-				ConsoleHref:        "www.example.com",
-				ApiHref:            "www.example.com",
-				LocalResourceId:    "01234",
-				ReporterVersion:    "0.1",
-			},
-		},
-	}
-	opts := getCallOptions()
-	_, err = client.K8sClusterService.CreateK8SCluster(context.Background(), &request, opts...)
-	assert.NoError(t, err)
-}
-
-func TestInventoryAPIHTTP_K8SCluster_UpdateK8SCluster(t *testing.T) {
-	t.Parallel()
-	c := v1beta1.NewConfig(
-		v1beta1.WithHTTPUrl(inventoryapi_http_url),
-		v1beta1.WithTLSInsecure(insecure),
-		v1beta1.WithHTTPTLSConfig(tlsConfig),
-	)
-	client, err := v1beta1.NewHttpClient(context.Background(), c)
-	if err != nil {
-		t.Error(err)
-	}
-	request := resources.UpdateK8SClusterRequest{
-		K8SCluster: &resources.K8SCluster{
-			Metadata: &resources.Metadata{
-				ResourceType: "k8s-cluster",
 				WorkspaceId:  "workspace1",
 				OrgId:        "",
 			},
@@ -309,51 +233,66 @@ func TestInventoryAPIHTTP_K8SCluster_UpdateK8SCluster(t *testing.T) {
 		},
 	}
 	opts := getCallOptions()
-	_, err = client.K8sClusterService.UpdateK8SCluster(context.Background(), &request, opts...)
-	assert.NoError(t, err)
+	_, err = client.K8sClusterService.CreateK8SCluster(context.Background(), &request, opts...)
+	assert.NoError(t, err, "Failed to create K8sCluster")
 
-}
-
-func TestInventoryAPIHTTP_K8SPolicy_CreateK8SPolicy(t *testing.T) {
-	t.Parallel()
-	c := v1beta1.NewConfig(
-		v1beta1.WithHTTPUrl(inventoryapi_http_url),
-		v1beta1.WithTLSInsecure(insecure),
-		v1beta1.WithHTTPTLSConfig(tlsConfig),
-	)
-	client, err := v1beta1.NewHttpClient(context.Background(), c)
-	if err != nil {
-		t.Error(err)
-	}
-
-	request := resources.CreateK8SPolicyRequest{
-		K8SPolicy: &resources.K8SPolicy{
+	updateRequest := resources.UpdateK8SClusterRequest{
+		K8SCluster: &resources.K8SCluster{
 			Metadata: &resources.Metadata{
-				ResourceType: "k8s_policy",
-				WorkspaceId:  "default",
+				ResourceType: "k8s_cluster",
+				WorkspaceId:  "workspace1",
 				OrgId:        "",
 			},
-			ResourceData: &resources.K8SPolicyDetail{
-				Disabled: false,
-				Severity: resources.K8SPolicyDetail_HIGH,
+			ResourceData: &resources.K8SClusterDetail{
+				ExternalClusterId: "01234",
+				ClusterStatus:     resources.K8SClusterDetail_READY,
+				KubeVersion:       "1.31",
+				KubeVendor:        resources.K8SClusterDetail_OPENSHIFT,
+				VendorVersion:     "4.16",
+				CloudPlatform:     resources.K8SClusterDetail_AWS_UPI,
+				Nodes: []*resources.K8SClusterDetailNodesInner{
+					{
+						Name:   "www.web.com",
+						Cpu:    "7500m",
+						Memory: "30973224Ki",
+						Labels: []*resources.ResourceLabel{
+							{
+								Key:   "has_monster_gpu",
+								Value: "no",
+							},
+						},
+					},
+				},
 			},
 			ReporterData: &resources.ReporterData{
 				ReporterInstanceId: "user@example.com",
-				ReporterType:       resources.ReporterData_OCM,
+				ReporterType:       resources.ReporterData_ACM,
 				ConsoleHref:        "www.example.com",
 				ApiHref:            "www.example.com",
-				LocalResourceId:    "012345",
+				LocalResourceId:    "01234",
 				ReporterVersion:    "0.1",
 			},
 		},
 	}
-	opts := getCallOptions()
-	_, err = client.PolicyServiceClient.CreateK8SPolicy(context.Background(), &request, opts...)
-	assert.NoError(t, err)
 
+	_, err = client.K8sClusterService.UpdateK8SCluster(context.Background(), &updateRequest, opts...)
+	assert.NoError(t, err, "Failed to update K8sCluster")
+
+	deleteRequest := resources.DeleteK8SClusterRequest{
+		ReporterData: &resources.ReporterData{
+			ReporterInstanceId: "user@example.com",
+			ReporterType:       resources.ReporterData_ACM,
+			ConsoleHref:        "www.example.com",
+			ApiHref:            "www.example.com",
+			LocalResourceId:    "01234",
+			ReporterVersion:    "0.1",
+		},
+	}
+	_, err = client.K8sClusterService.DeleteK8SCluster(context.Background(), &deleteRequest, opts...)
+	assert.NoError(t, err, "Failed to delete K8sCluster")
 }
 
-func TestInventoryAPIHTTP_K8SPolicy_UpdateK8SPolicy(t *testing.T) {
+func TestInventoryAPIHTTP_K8SPolicyLifecycle(t *testing.T) {
 	t.Parallel()
 	c := v1beta1.NewConfig(
 		v1beta1.WithHTTPUrl(inventoryapi_http_url),
@@ -364,11 +303,10 @@ func TestInventoryAPIHTTP_K8SPolicy_UpdateK8SPolicy(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-
-	request := resources.UpdateK8SPolicyRequest{
+	request := resources.CreateK8SPolicyRequest{
 		K8SPolicy: &resources.K8SPolicy{
 			Metadata: &resources.Metadata{
-				ResourceType: "k8s-policy",
+				ResourceType: "k8s_policy",
 				WorkspaceId:  "workspace2",
 				OrgId:        "",
 			},
@@ -387,9 +325,46 @@ func TestInventoryAPIHTTP_K8SPolicy_UpdateK8SPolicy(t *testing.T) {
 		},
 	}
 	opts := getCallOptions()
-	_, err = client.PolicyServiceClient.UpdateK8SPolicy(context.Background(), &request, opts...)
-	assert.NoError(t, err)
+	_, err = client.PolicyServiceClient.CreateK8SPolicy(context.Background(), &request, opts...)
+	assert.NoError(t, err, "Failed to create K8sPolicy")
 
+	updateRequest := resources.UpdateK8SPolicyRequest{
+		K8SPolicy: &resources.K8SPolicy{
+			Metadata: &resources.Metadata{
+				ResourceType: "k8s_policy",
+				WorkspaceId:  "workspace2",
+				OrgId:        "",
+			},
+			ResourceData: &resources.K8SPolicyDetail{
+				Disabled: true,
+				Severity: resources.K8SPolicyDetail_LOW,
+			},
+			ReporterData: &resources.ReporterData{
+				ReporterInstanceId: "user@example.com",
+				ReporterType:       resources.ReporterData_OCM,
+				ConsoleHref:        "www.example.com",
+				ApiHref:            "www.example.com",
+				LocalResourceId:    "012345",
+				ReporterVersion:    "0.1",
+			},
+		},
+	}
+
+	_, err = client.PolicyServiceClient.UpdateK8SPolicy(context.Background(), &updateRequest, opts...)
+	assert.NoError(t, err, "Failed to update K8sPolicy")
+
+	deleteRequest := resources.DeleteK8SPolicyRequest{
+		ReporterData: &resources.ReporterData{
+			ReporterInstanceId: "user@example.com",
+			ReporterType:       resources.ReporterData_OCM,
+			ConsoleHref:        "www.example.com",
+			ApiHref:            "www.example.com",
+			LocalResourceId:    "012345",
+			ReporterVersion:    "0.1",
+		},
+	}
+	_, err = client.PolicyServiceClient.DeleteK8SPolicy(context.Background(), &deleteRequest, opts...)
+	assert.NoError(t, err, "Failed to delete K8sPolicy")
 }
 
 func getCallOptions() []http.CallOption {

cbde528e1cccb778555c4b5b7fb250ccdeb6ded6
Adam0Brien <aobrien@redhat.com>
2024-11-19 10:49:23 +0000

    relations/spicedb/inventory setup for e2e testing

---
+++ deploy/kind/relations/spicedb-kind-setup/setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -oe errexit
+
+if ! command -v kind &> /dev/null
+then
+    echo "kind could not be found"
+    exit
+fi
+
+if ! command -v helm &> /dev/null
+then
+    echo "helm could not be found"
+    exit
+fi
+
+if ! command -v kubectl &> /dev/null
+then
+    echo "kubectl could not be found"
+    exit
+fi
+
+# desired cluster name; default is "kind"
+echo "creating kind cluster"
+
+kind create cluster --config deploy/kind/relations/spicedb-kind-setup/kind-kube/kind-ingress.config
+
+echo "> waiting for kubernetes node(s) become ready"
+kubectl wait --for=condition=ready node --all --timeout=60s
+
+echo "deploy contour"
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/kind-kube/contour.yaml
+
+
+echo "Install spicedb-operator"
+kubectl create namespace spicedb-operator
+kubectl apply --server-side -f https://github.com/authzed/spicedb-operator/releases/latest/download/bundle.yaml -n spicedb-operator
+
+echo "create spicedb namespace"
+kubectl create namespace spicedb
+echo "deploy postgres"
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/postgres/secret.yaml -n spicedb
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/postgres/storage.yaml -n spicedb
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/postgres/postgresql.yaml -n spicedb
+
+echo "deploy spicedb"
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/spicedb-cr.yaml -n spicedb
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/svc-ingress.yaml -n spicedb
+
+while [[ -z $(kubectl get deployments.apps -n spicedb spicedb-cr-spicedb -o jsonpath="{.status.readyReplicas}" 2>/dev/null) ]]; do
+  echo "still waiting for spicedb"
+  sleep 1
+done
+echo "spicedb is ready"
+kubectl get ingresses.networking.k8s.io -n spicedb
+
+echo "Deploying relations-api service"
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/relations-api/secret.yaml -n spicedb
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/relations-api/deployment.yaml -n spicedb
+kubectl apply -f ./deploy/kind/relations/spicedb-kind-setup/relations-api/svc.yaml -n spicedb
+
+while [[ -z $(kubectl get deployments.apps -n spicedb relationships -o jsonpath="{.status.readyReplicas}" 2>/dev/null) ]]; do
+  echo "still waiting for relationships"
+  sleep 1
+done
+
+echo "Route"
+kubectl get ingresses.networking.k8s.io -n spicedb
+
+echo "Relations - Write(POST) - Sample CURL request"
+echo ""
+JSON_DATA='{ "tuples": [{"resource": {"type": {"type": "group"},"id": "bob_club2"},"relation": "member","subject": {"subject": {"type": {"type": "user"},"id": "bob2"}}}]}'
+echo "curl -v http://relationships.127.0.0.1.nip.io:8000/api/authz/v1beta1/tuples -H 'Content-Type: application/json' -d '$JSON_DATA'"
--- scripts/start-inventory-kind.sh
+++ scripts/start-inventory-kind.sh
@@ -21,10 +21,39 @@ ${DOCKER} save -o inventory-e2e-tests.tar localhost/inventory-e2e-tests:e2e-test
 kind load image-archive inventory-api.tar --name inventory-cluster
 kind load image-archive inventory-e2e-tests.tar --name inventory-cluster
 
+#kubectl create configmap inventory-api-psks --from-file=config/psks.yaml
+#
+#kubectl apply -f https://strimzi.io/install/latest\?namespace\=default
+#kubectl apply -f deploy/kind/inventory/kessel-inventory.yaml
+#kubectl apply -f deploy/kind/inventory/invdatabase.yaml
+#kubectl apply -f deploy/kind/e2e/e2e-batch.yaml
+#kubectl apply -f deploy/kind/inventory/strimzi.yaml
+
+# Create the kessel namespace
+kubectl create namespace kessel
+
+# Deploy ConfigMap for inventory-api
 kubectl create configmap inventory-api-psks --from-file=config/psks.yaml
 
+# Deploy Inventory dependencies
 kubectl apply -f https://strimzi.io/install/latest\?namespace\=default
 kubectl apply -f deploy/kind/inventory/kessel-inventory.yaml
 kubectl apply -f deploy/kind/inventory/invdatabase.yaml
-kubectl apply -f deploy/kind/e2e/e2e-batch.yaml
 kubectl apply -f deploy/kind/inventory/strimzi.yaml
+
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/bundle.yaml
+
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+kubectl get crd httpproxies.projectcontour.io
+
+# Deploy SpiceDB and Relations-API in the kessel namespace
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/postgres/secret.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/postgres/postgresql.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/postgres/storage.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/spicedb-cr.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/svc-ingress.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/relations-api/secret.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/relations-api/deployment.yaml
+kubectl apply -f deploy/kind/relations/spicedb-kind-setup/relations-api/svc.yaml
+
+echo "Setup complete. Inventory API, Relations-API, and SpiceDB are running!"

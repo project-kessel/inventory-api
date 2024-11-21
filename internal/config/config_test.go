cf5831ea640c6cb2b7f85770f8f3939655fd809a
Antony Natale <anatale@redhat.com>
2024-11-21 09:37:16 -0500

    update config to capture the RDS CA cert when available and ensure SSL connections to db

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,43 +1,90 @@
 package config
 
 import (
+	"os"
+	"testing"
+
 	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
+func ParseCA(t *testing.T, path string) string {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		t.Errorf("failed to read file %s: %s", path, err)
+		return ""
+	}
+	return string(file)
+}
+
 func TestConfigureStorage(t *testing.T) {
-	tests := []struct {
+	var testCA = `-----BEGIN CERTIFICATE-----\nFAKE-CA\n-----END CERTIFICATE-----`
+
+	test := struct {
 		name      string
 		appconfig *clowder.AppConfig
 		options   *OptionsConfig
 	}{
-		{
-			name: "ensures DB info is set",
-			appconfig: &clowder.AppConfig{
-				Database: &clowder.DatabaseConfig{
-					Hostname: "postgres",
-					Name:     "postgres",
-					Port:     5432,
-					Username: "db-user",
-					Password: "db-password",
-				},
+		name: "ensures DB info is set",
+		appconfig: &clowder.AppConfig{
+			Database: &clowder.DatabaseConfig{
+				Hostname: "postgres",
+				Name:     "postgres",
+				Port:     5432,
+				Username: "db-user",
+				Password: "db-password",
+				RdsCa:    &testCA,
 			},
-			options: NewOptionsConfig(),
 		},
+		options: NewOptionsConfig(),
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			test.options.ConfigureStorage(test.appconfig.Database)
-			assert.Equal(t, "postgres", test.options.Storage.Database)
-			assert.Equal(t, "postgres", test.options.Storage.Postgres.Host)
-			assert.Equal(t, "postgres", test.options.Storage.Postgres.DbName)
-			assert.Equal(t, "5432", test.options.Storage.Postgres.Port)
-			assert.Equal(t, "db-user", test.options.Storage.Postgres.User)
-			assert.Equal(t, "db-password", test.options.Storage.Postgres.Password)
-		})
+	t.Run(test.name, func(t *testing.T) {
+		err := test.options.ConfigureStorage(test.appconfig)
+		assert.Nil(t, err)
+
+		assert.NotNil(t, test.options.Storage.Postgres.SSLRootCert)
+		contents := ParseCA(t, test.options.Storage.Postgres.SSLRootCert)
+		assert.Equal(t, testCA, contents)
+
+		assert.Equal(t, "postgres", test.options.Storage.Database)
+		assert.Equal(t, "postgres", test.options.Storage.Postgres.Host)
+		assert.Equal(t, "postgres", test.options.Storage.Postgres.DbName)
+		assert.Equal(t, "5432", test.options.Storage.Postgres.Port)
+		assert.Equal(t, "db-user", test.options.Storage.Postgres.User)
+		assert.Equal(t, "db-password", test.options.Storage.Postgres.Password)
+	})
+
+	testNoCA := struct {
+		name      string
+		appconfig *clowder.AppConfig
+		options   *OptionsConfig
+	}{
+		name: "ensures DB info is set when no CA cert is provided",
+		appconfig: &clowder.AppConfig{
+			Database: &clowder.DatabaseConfig{
+				Hostname: "postgres",
+				Name:     "postgres",
+				Port:     5432,
+				Username: "db-user",
+				Password: "db-password",
+			},
+		},
+		options: NewOptionsConfig(),
 	}
+
+	t.Run(testNoCA.name, func(t *testing.T) {
+		err := testNoCA.options.ConfigureStorage(testNoCA.appconfig)
+		assert.Nil(t, err)
+		assert.Equal(t, testNoCA.options.Storage.Postgres.SSLRootCert, "")
+
+		assert.Equal(t, "postgres", testNoCA.options.Storage.Database)
+		assert.Equal(t, "postgres", testNoCA.options.Storage.Postgres.Host)
+		assert.Equal(t, "postgres", testNoCA.options.Storage.Postgres.DbName)
+		assert.Equal(t, "5432", testNoCA.options.Storage.Postgres.Port)
+		assert.Equal(t, "db-user", testNoCA.options.Storage.Postgres.User)
+		assert.Equal(t, "db-password", testNoCA.options.Storage.Postgres.Password)
+	})
 }
 
 func TestConfigureAuthz(t *testing.T) {

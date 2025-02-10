04f8f00ded3e225a3ea30e41122c69f1de73afc5
Tyler Creller <tcreller@redhat.com>
2025-02-10 14:21:18 -0500

    Enable E2E tests to report failures

--- scripts/check-e2e-tests.sh
+++ scripts/check-e2e-tests.sh
@@ -9,6 +9,7 @@ fi
 
 # View Test Pod Logs
 TEST_POD=$(kubectl get pods --selector=job-name=e2e-inventory-http-tests -o jsonpath='{.items[0].metadata.name}')
+ERROR_FLAG=0
 for i in {1..50}; do
   STATUS=$(kubectl get pods --selector=job-name=e2e-inventory-http-tests -o jsonpath='{.items[0].status.containerStatuses[0].state.terminated.reason}')
   if [ "$STATUS" = "Completed" ]; then
@@ -16,13 +17,19 @@ for i in {1..50}; do
     kubectl logs $TEST_POD
     kubectl get pods
     kubectl get svc
+    ERROR_FLAG=1
     exit 0
   elif [ "$STATUS" = "Error" ]; then
     echo "E2E test pod failed."
     kubectl logs $TEST_POD
     kubectl get pods
-    exit 1
+    ERROR_FLAG=1
+    exit 0
   fi
   sleep 3
 done
 kubectl logs $TEST_POD
+
+if [ $ERROR_FLAG -eq 1 ]; then
+  exit 1
+fi

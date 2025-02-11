1fb691fc4e9250363394e84884147c2b66168813
Jonathan Marcantonio <jmarcant@redhat.com>
2025-02-11 10:54:34 -0500

    [WIP] add checkservice endpoints for zookie managed soln
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>

--- internal/authz/api/authz-service.go
+++ internal/authz/api/authz-service.go
@@ -2,13 +2,17 @@ package api
 
 import (
 	"context"
+
+	inv "github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta1/relations"
 	kesselv1 "github.com/project-kessel/relations-api/api/kessel/relations/v1"
 	kessel "github.com/project-kessel/relations-api/api/kessel/relations/v1beta1"
 )
 
 type Authorizer interface {
 	Health(ctx context.Context) (*kesselv1.GetReadyzResponse, error)
-	Check(context.Context, *kessel.CheckRequest) (*kessel.CheckResponse, error)
+	CheckForView(context.Context, *inv.CheckForViewRequest) (*inv.CheckForViewResponse, error)
+	CheckForUpdate(context.Context, *inv.CheckForUpdateRequest) (*inv.CheckForUpdateResponse, error)
+	CheckForCreate(context.Context, *inv.CheckForCreateRequest) (*inv.CheckForCreateResponse, error)
 	CreateTuples(context.Context, *kessel.CreateTuplesRequest) (*kessel.CreateTuplesResponse, error)
 	DeleteTuples(context.Context, *kessel.DeleteTuplesRequest) (*kessel.DeleteTuplesResponse, error)
 	UnsetWorkspace(context.Context, string, string, string) (*kessel.DeleteTuplesResponse, error)

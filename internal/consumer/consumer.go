af9a073dad53a7cd3840d195dd5aec8908011e79
Antony Natale <anatale@redhat.com>
2025-03-17 13:52:50 -0400

    adds basic logic to: * parse the message and unmarshal into a CreateTupleRequest * calls Relations API to create the tuple * captures and logs the consistency token from the response
    It also updates the relations api in go.mod to make GetConsistencyToken
    call available

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -1,11 +1,14 @@
 package consumer
 
 import (
+	"context"
+	"encoding/json"
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/project-kessel/inventory-api/internal/authz"
 	"github.com/project-kessel/inventory-api/internal/authz/api"
 	"github.com/project-kessel/inventory-api/internal/storage"
+	kessel "github.com/project-kessel/relations-api/api/kessel/relations/v1beta1"
 	"os"
 	"os/signal"
 	"syscall"
@@ -60,17 +63,28 @@ func (i *InventoryConsumer) Consume() error {
 			i.Logger.Infof("Caught signal %v: terminating\n", sig)
 			run = false
 		default:
-			// Most examples seem to use Poll() and batch commit after a specific
-			// number of messages. This example does so after every message
+			/* TODO this is certainly not production quality
+			this should move to using poll and potentially capture
+			the number of messages we've fetched before commiting vs
+			commiting on every message
+
+			also need to account for other types of messages like when rebalances occur
+			rebalance logic should ensure things are committoed before partitions are lost
+			*/
 			ev, err := i.Consumer.ReadMessage(100 * time.Millisecond)
 			if err != nil {
 				// Errors are informational and automatically handled by the consumer
 				continue
 			}
-			//err = i.WriteDB(ev)
-			//if err != nil {
-			//	i.Logger.Infof("Failed to write to db: %v", err)
-			//}
+			resp, err := i.CreateTuple(context.Background(), ev)
+			if err != nil {
+				i.Logger.Infof("Failed to create tuple: %v", err)
+			}
+
+			// Just logging the token till i implement the DB actions here
+			i.Logger.Infof("Tuple Token: %v", resp)
+
+			// See note above about commiting on every run
 			_, err = i.Consumer.Commit()
 			if err != nil {
 				i.Logger.Errorf("Error on commit: %v", err)
@@ -86,7 +100,32 @@ func (i *InventoryConsumer) Consume() error {
 	return nil
 }
 
+// CreateTuple calls the Relations API to create a tuple from the message payload received and returns the consistency token
+func (i *InventoryConsumer) CreateTuple(ctx context.Context, msg *kafka.Message) (string, error) {
+
+	var rels []*kessel.Relationship
+	var tuple *kessel.Relationship
+
+	err := json.Unmarshal(msg.Value, &tuple)
+	if err != nil {
+		return "", err
+	}
+	rels = append(rels, tuple)
+
+	resp, err := i.Authorizer.CreateTuples(ctx, &kessel.CreateTuplesRequest{
+		Tuples: rels,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	return resp.GetConsistencyToken().Token, nil
+}
+
+// Shutdown ensures the consumer is properly shutdown, whether by server or due to rebalance
 func (i *InventoryConsumer) Shutdown() error {
+	// TODO, shutting down the consumer should attempt to commit the offset if we've processed the message
+	// for now it just stops the consumer connection
 	err := i.Consumer.Close()
 	if err != nil {
 		i.Logger.Errorf("Error closing kafka consumer: %v", err)

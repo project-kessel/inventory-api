b723265999b4b2a6e2be8ec8333519b588262808
Antony Natale <anatale@redhat.com>
2025-03-18 16:35:21 -0400

    Updates to consume loop: * switchs to poll to capture events and messages * adds switch statement to execute based on event type * updates function parameters to take message data from event

--- internal/consumer/consumer.go
+++ internal/consumer/consumer.go
@@ -4,138 +4,138 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/project-kessel/inventory-api/internal/biz/model"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/project-kessel/inventory-api/internal/authz"
 	"github.com/project-kessel/inventory-api/internal/authz/api"
+	"github.com/project-kessel/inventory-api/internal/biz/model"
 	kessel "github.com/project-kessel/relations-api/api/kessel/relations/v1beta1"
 	"gorm.io/gorm"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 // InventoryConsumer defines a Kafka Consumer with required clients and configs to call Relations API and update the Inventory DB with consistency tokens
 type InventoryConsumer struct {
-	Consumer            *kafka.Consumer
-	Config              CompletedConfig
-	DB                  *gorm.DB
-	PersistenceDisabled bool
-	AuthzConfig         authz.CompletedConfig
-	Authorizer          api.Authorizer
-	Logger              *log.Helper
+	Consumer    *kafka.Consumer
+	Config      CompletedConfig
+	DB          *gorm.DB
+	AuthzConfig authz.CompletedConfig
+	Authorizer  api.Authorizer
+	Logger      *log.Helper
 }
 
-// KeyPayload stores the event key data captured from the topic as emitted by Debezium
+// KeyPayload stores the event message key captured from the topic as emitted by Debezium
 type KeyPayload struct {
 	MessageSchema map[string]interface{} `json:"schema"`
 	InventoryID   string                 `json:"payload"`
 }
 
-// MessagePayload stores the event message captured from the topic as emitted by Debezium
+// MessagePayload stores the event message value captured from the topic as emitted by Debezium
 type MessagePayload struct {
 	MessageSchema  map[string]interface{} `json:"schema"`
 	RelationsTuple interface{}            `json:"payload"`
 }
 
 // New instantiates a new InventoryConsumer
-func New(config CompletedConfig, db *gorm.DB, persistanceDisabled bool, authz authz.CompletedConfig, authorizer api.Authorizer, logger *log.Helper) (InventoryConsumer, error) {
+func New(config CompletedConfig, db *gorm.DB, authz authz.CompletedConfig, authorizer api.Authorizer, logger *log.Helper) (InventoryConsumer, error) {
 	logger.Info("Setting up kafka consumer")
 	consumer, err := kafka.NewConsumer(config.KafkaConfig)
 	if err != nil {
-		logger.Errorf("Error creating kafka consumer: %v", err)
+		logger.Errorf("error creating kafka consumer: %v", err)
 		return InventoryConsumer{}, err
 	}
 
 	return InventoryConsumer{
-		Consumer:            consumer,
-		Config:              config,
-		DB:                  db,
-		PersistenceDisabled: persistanceDisabled,
-		AuthzConfig:         authz,
-		Authorizer:          authorizer,
-		Logger:              logger,
+		Consumer:    consumer,
+		Config:      config,
+		DB:          db,
+		AuthzConfig: authz,
+		Authorizer:  authorizer,
+		Logger:      logger,
 	}, nil
 }
 
 // Consume begins the consumption loop for the Consumer
 func (i *InventoryConsumer) Consume() error {
+	// TODO -- potentially leverage rebalanceCallback here to run something when rebalance occurs
+	// more specifically, if we start commiting after X number of messages, when consumer loses partition
+	// we can ensure to commit any offsets not yet commit. This is more futureproofing than critical for now
 	err := i.Consumer.SubscribeTopics([]string{i.Config.Topic}, nil)
 	if err != nil {
-		i.Logger.Errorf("Failed to subscribe to topic: %v", err)
+		i.Logger.Errorf("failed to subscribe to topic: %v", err)
 		return err
 	}
 
-	// Set up a channel for handling exiting pods
+	// Set up a channel for handling exiting pods or ctrl+c
 	sigchan := make(chan os.Signal, 1)
 	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
 	// Process messages
 	run := true
-	i.Logger.Info("consumer ready: waiting for messages...")
+	i.Logger.Info("Consumer ready: waiting for messages...")
 	for run {
 		select {
 		case sig := <-sigchan:
-			i.Logger.Infof("Caught signal %v: terminating\n", sig)
+			i.Logger.Infof("caught signal %v: terminating\n", sig)
 			run = false
 		default:
-			/* TODO
-			This is certainly not production quality
-			this should move to using poll and potentially capture
-			the number of messages we've fetched before commiting vs
-			commiting on every msg
-
-			Also need to account for other types of messages like when rebalances occur
-			rebalance logic should ensure things are committed before partitions are lost
-			*/
-			msg, err := i.Consumer.ReadMessage(100 * time.Millisecond)
-			if err != nil {
+			event := i.Consumer.Poll(100)
+			if event == nil {
 				continue
 			}
 
-			resp, err := i.CreateTuple(context.Background(), msg)
-			if err != nil {
-				i.Logger.Infof("Failed to create tuple: %v", err)
-				continue
-			}
-
-			if !i.PersistenceDisabled {
-				// With this logic, msg.Key is expected to be the InventoryID of the resource in Inventory DB
-				// and will correlate to aggregateid in the outbox table
-				err = i.UpdateConsistencyToken(msg, resp)
+			switch e := event.(type) {
+			case *kafka.Message:
+				// TODO, failures due to tuples already existing should not prevent moving on to the next message,
+				// need some idempotency for message replays
+				resp, err := i.CreateTuple(context.Background(), e.Value)
 				if err != nil {
-					i.Logger.Infof("Failed to update consistency token: %v", err)
+					i.Logger.Infof("failed to create tuple: %v", err)
 					continue
 				}
-			}
 
-			// See note above about commiting on every run
-			_, err = i.Consumer.Commit()
-			if err != nil {
-				i.Logger.Errorf("Error on commit: %v", err)
-				continue
+				err = i.UpdateConsistencyToken(e.Key, resp)
+				if err != nil {
+					i.Logger.Infof("failed to update consistency token: %v", err)
+					continue
+				}
+
+				// TODO: Commiting on every message is not ideal - we will need to revisit this as we consume more messages
+				// Potentially commit ever X number of messages or use an arbitrary value like:
+				// if topicPartition.Offset%10 == 0 { err := c.Commit() }
+				_, err = i.Consumer.Commit()
+				if err != nil {
+					i.Logger.Errorf("error on commit: %v", err)
+					continue
+				}
+				i.Logger.Infof("consumed event from topic %s, partition %d at offset %s: key = %-10s value = %s\n",
+					*e.TopicPartition.Topic, e.TopicPartition.Partition, e.TopicPartition.Offset, string(e.Key), string(e.Value))
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% error: %v: %v\n", e.Code(), e)
+				if e.Code() == kafka.ErrAllBrokersDown {
+					run = false
+				}
+			default:
+				fmt.Printf("event type ignored %v\n", e)
 			}
-			i.Logger.Infof("Consumed event from topic %s, partition %d at offset %s: key = %-10s value = %s\n",
-				*msg.TopicPartition.Topic, msg.TopicPartition.Partition, msg.TopicPartition.Offset, string(msg.Key), string(msg.Value))
 		}
 	}
 	err = i.Shutdown()
 	if err != nil {
-		return err
+		return fmt.Errorf("error in consumer shutdown: %v", err)
 	}
 	return nil
 }
 
 // CreateTuple calls the Relations API to create a tuple from the message payload received and returns the consistency token
-func (i *InventoryConsumer) CreateTuple(ctx context.Context, msg *kafka.Message) (string, error) {
+func (i *InventoryConsumer) CreateTuple(ctx context.Context, msg []byte) (string, error) {
 	var msgPayload *MessagePayload
 	var tuple *kessel.Relationship
 
-	// As it stands, msg.Value is expected to be a valid JSON body for a single relation
-	err := json.Unmarshal(msg.Value, &msgPayload)
+	// msg value is expected to be a valid JSON body for a single relation
+	err := json.Unmarshal(msg, &msgPayload)
 	if err != nil {
 		return "", fmt.Errorf("error unmarshalling msgPayload: %v", err)
 	}
@@ -151,26 +151,23 @@ func (i *InventoryConsumer) CreateTuple(ctx context.Context, msg *kafka.Message)
 	if err != nil {
 		return "", fmt.Errorf("error creating tuple: %v", err)
 	}
-	i.Logger.Infof("Tuple Created: %v", tuple)
+	i.Logger.Infof("tuple created: %+v", tuple)
+
 	return resp.GetConsistencyToken().Token, nil
 }
 
 // UpdateConsistencyToken updates the resource in the inventory DB to add the consistency token
-func (i *InventoryConsumer) UpdateConsistencyToken(msg *kafka.Message, token string) error {
-	//var resource model.Resource
+func (i *InventoryConsumer) UpdateConsistencyToken(msg []byte, token string) error {
 	var msgPayload *KeyPayload
 
-	err := json.Unmarshal(msg.Key, &msgPayload)
+	// msg key is expected to be the inventory_id of a resource
+	err := json.Unmarshal(msg, &msgPayload)
 	if err != nil {
 		return fmt.Errorf("error unmarshalling msgPayload: %v", err)
 	}
 
-	i.Logger.Infof("InventoryID is %s", msgPayload.InventoryID)
-	//i.DB.First(&resource, "inventory_id = ?", msgPayload.Payload)
-
-	//resource.ConsistencyToken = token
+	// this will update all records for the same inventory_id with current consistency token
 	i.DB.Model(model.Resource{}).Where("inventory_id = ?", msgPayload.InventoryID).Update("consistency_token", token)
-	//i.DB.Save(&resource)
 	return nil
 }
 

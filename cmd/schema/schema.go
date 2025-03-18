e7ef11bbcff7ab4c20efe678d1c27c4962e3e245
Adam0Brien <aobrien@redhat.com>
2025-03-18 14:24:52 +0000

    change schema loading from in code to command line argument

---
+++ cmd/schema/schema.go
@@ -0,0 +1,148 @@
+package schema
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/project-kessel/inventory-api/cmd/common"
+	"github.com/project-kessel/inventory-api/internal/middleware"
+	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
+)
+
+var schemaDir = "data/schema/resources"
+
+const schemaCacheFile = "schema_cache.json"
+
+// SchemaCache stores the structured schema data
+var schemaCache = make(map[string]interface{})
+
+// Read JSON file
+func readJSONFile(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+// Encode YAML file content to Base64
+func encodeYAMLToBase64(filePath string) (string, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+	encoded := base64.StdEncoding.EncodeToString(data)
+	return encoded, nil
+}
+
+// Preload schemas
+func preloadSchemas() error {
+
+	resources, err := os.ReadDir(schemaDir)
+	if err != nil {
+		return fmt.Errorf("failed to read schema directory: %w", err)
+	}
+
+	for _, resource := range resources {
+		if !resource.IsDir() {
+			continue
+		}
+
+		resourceType := middleware.NormalizeResourceType(resource.Name())
+		resourcePath := filepath.Join(schemaDir, resource.Name())
+
+		// Load common resource data schema
+		commonSchemaPath := filepath.Join(resourcePath, "common_resource_data.json")
+		if _, err := os.Stat(commonSchemaPath); err == nil {
+			if jsonData, err := readJSONFile(commonSchemaPath); err == nil {
+				schemaCache[fmt.Sprintf("common:%s", resourceType)] = jsonData
+			}
+		}
+
+		// Load config.yaml and encode in Base64
+		configPath := filepath.Join(resourcePath, "config.yaml")
+		if _, err := os.Stat(configPath); err == nil {
+			if encodedConfig, err := encodeYAMLToBase64(configPath); err == nil {
+				schemaCache[fmt.Sprintf("config:%s", resourceType)] = encodedConfig
+			}
+		}
+
+		// Process reporter schemas
+		reportersPath := filepath.Join(resourcePath, "reporters")
+		if reporterDirs, err := os.ReadDir(reportersPath); err == nil {
+			for _, reporter := range reporterDirs {
+				if !reporter.IsDir() {
+					continue
+				}
+
+				reporterType := middleware.NormalizeResourceType(reporter.Name())
+				reporterPath := filepath.Join(reportersPath, reporter.Name())
+
+				// Encode reporter's config.yaml
+				reporterConfigPath := filepath.Join(reporterPath, "config.yaml")
+				if _, err := os.Stat(reporterConfigPath); err == nil {
+					if encodedConfig, err := encodeYAMLToBase64(reporterConfigPath); err == nil {
+						schemaCache[fmt.Sprintf("config:%s:%s", resourceType, reporterType)] = encodedConfig
+					}
+				}
+
+				// Load reporter-specific schema JSON
+				reporterSchemaPath := filepath.Join(reporterPath, fmt.Sprintf("%s.json", resource.Name()))
+				if _, err := os.Stat(reporterSchemaPath); err == nil {
+					if jsonData, err := readJSONFile(reporterSchemaPath); err == nil {
+						schemaCache[fmt.Sprintf("%s:%s", resourceType, reporterType)] = jsonData
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+// Save schemaCache to a JSON file
+func saveSchemaCache() error {
+	data, err := json.MarshalIndent(schemaCache, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal schema cache: %w", err)
+	}
+
+	if err := os.WriteFile(schemaCacheFile, data, 0644); err != nil {
+		return fmt.Errorf("failed to write schema cache to file: %w", err)
+	}
+
+	log.Infof("Schema cache successfully written to %s", schemaCacheFile)
+	return nil
+}
+
+// NewCommand creates a new Cobra command for schema preloading
+func NewCommand(loggerOptions common.LoggerOptions) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "preload-schema",
+		Short: "Preload schema cache from filesystem",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			_, logger := common.InitLogger(common.GetLogLevel(), loggerOptions)
+			logHelper := log.NewHelper(log.With(logger, "subsystem", "schema"))
+
+			// Preload schemas
+			if err := preloadSchemas(); err != nil {
+				logHelper.Errorf("Error preloading schemas: %v", err)
+				return err
+			}
+
+			// Save schema cache
+			if err := saveSchemaCache(); err != nil {
+				logHelper.Errorf("Error saving schema cache: %v", err)
+				return err
+			}
+
+			logHelper.Info("Schema cache updated successfully.")
+			return nil
+		},
+	}
+
+	return cmd
+}
--- internal/middleware/preload_schema.go
+++ internal/middleware/preload_schema.go
@@ -82,12 +82,6 @@ func PreloadAllSchemasFromFilesystem(resourceDir string) error {
 		}
 	}
 
-	// E2E tests will not run with this enabled
-	//schemaCachePath := filepath.Join("./schema_cache.json")
-	//if err := DumpSchemaCacheToJSON(schemaCachePath); err != nil {
-	//	log.Errorf("Failed to dump schema cache to JSON: %v", err)
-	//	return err
-	//}
 	return nil
 }
 
@@ -143,25 +137,3 @@ func loadConfigFile(resourceDir string, resourceType string) (struct {
 	schemaCache.Store(fmt.Sprintf("config:%s", configResourceType), configData)
 	return config, nil
 }
-
-//// DumpSchemaCacheToJSON saves the schema cache to a JSON file
-//func DumpSchemaCacheToJSON(filePath string) error {
-//	cacheMap := make(map[string]interface{})
-//
-//	schemaCache.Range(func(key, value interface{}) bool {
-//		cacheMap[key.(string)] = value
-//		return true
-//	})
-//
-//	jsonData, err := json.MarshalIndent(cacheMap, "", "  ")
-//	if err != nil {
-//		return fmt.Errorf("failed to marshal schema cache: %w", err)
-//	}
-//
-//	err = os.WriteFile(filePath, jsonData, 0644)
-//	if err != nil {
-//		return fmt.Errorf("failed to write schema cache to file: %w", err)
-//	}
-//
-//	return nil
-//}

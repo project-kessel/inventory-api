9a8e5446134280f9b1518c7657a38f23b65bafae
Antony Natale <43508092+tonytheleg@users.noreply.github.com>
2025-03-24 14:11:19 -0400

    Ephem deploy update w consumer (#398)
    * updates to ephem deploy process for feature branch
    
    * adds missing error check logic in consumer lost during merge

--- cmd/serve/serve.go
+++ cmd/serve/serve.go
@@ -230,7 +230,10 @@ func NewCommand(
 
 			if !storageOptions.DisablePersistence {
 				go func() {
-					_ = inventoryConsumer.Consume()
+					err := inventoryConsumer.Consume()
+					if err != nil {
+						inventoryConsumer.Errors <- err
+					}
 				}()
 			}
 

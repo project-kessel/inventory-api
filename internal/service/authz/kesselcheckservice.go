2140eba78493dfe983f607f2ca71eba8efba3d56
Adam O'Brien <aobrien@redhat.com>
2025-03-27 08:14:38 +0000

    RHCLOUD-39017: Add kessel check service to v1beta2 (#409)

--- internal/service/authz/kesselcheckservice.go
+++ internal/service/authz/kesselcheckservice.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	pb "github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta1/authz"
+	pbv1beta2 "github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta2/authz"
 	authnapi "github.com/project-kessel/inventory-api/internal/authn/api"
 	"github.com/project-kessel/inventory-api/internal/biz/model"
 	resources "github.com/project-kessel/inventory-api/internal/biz/resources"
@@ -21,9 +22,20 @@ type KesselCheckServiceService struct {
 
 	Ctl *resources.Usecase
 }
+type KesselCheckServiceServiceV1beta2 struct {
+	pbv1beta2.UnimplementedKesselCheckServiceServer
+
+	Ctl *resources.Usecase
+}
 
 func New(c *resources.Usecase) *KesselCheckServiceService {
 	return &KesselCheckServiceService{
+		Ctl: c,
+	}
+}
+
+func NewV1beta2(c *resources.Usecase) *KesselCheckServiceServiceV1beta2 {
+	return &KesselCheckServiceServiceV1beta2{
 		Ctl: c,
 	}
 }
@@ -46,6 +58,32 @@ func (s *KesselCheckServiceService) Check(ctx context.Context, req *pb.CheckRequ
 			},
 		}, *resource); err == nil {
 			return viewResponseFromAuthzRequest(resp), nil
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
+}
+
+func (s *KesselCheckServiceServiceV1beta2) Check(ctx context.Context, req *pbv1beta2.CheckRequest) (*pbv1beta2.CheckResponse, error) {
+	identity, err := middleware.GetIdentity(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if resource, err := authzFromRequestV1beta2(identity, req.Parent); err == nil {
+		if resp, err := s.Ctl.Check(ctx, req.GetRelation(), req.Parent.Type.GetNamespace(), &v1beta1.SubjectReference{
+			Relation: req.GetSubject().Relation,
+			Subject: &v1beta1.ObjectReference{
+				Type: &v1beta1.ObjectType{
+					Namespace: req.GetSubject().GetSubject().GetType().GetNamespace(),
+					Name:      req.GetSubject().GetSubject().GetType().GetName(),
+				},
+				Id: req.GetSubject().GetSubject().GetId(),
+			},
+		}, *resource); err == nil {
+			return viewResponseFromAuthzRequestV1beta2(resp), nil
 		} else {
 			return nil, err
 		}
@@ -80,7 +118,42 @@ func (s *KesselCheckServiceService) CheckForUpdate(ctx context.Context, req *pb.
 	}
 }
 
+func (s *KesselCheckServiceServiceV1beta2) CheckForUpdate(ctx context.Context, req *pbv1beta2.CheckForUpdateRequest) (*pbv1beta2.CheckForUpdateResponse, error) {
+	identity, err := middleware.GetIdentity(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if resource, err := authzFromRequestV1beta2(identity, req.Parent); err == nil {
+		if resp, err := s.Ctl.CheckForUpdate(ctx, req.GetRelation(), req.Parent.Type.GetNamespace(), &v1beta1.SubjectReference{
+			Relation: req.GetSubject().Relation,
+			Subject: &v1beta1.ObjectReference{
+				Type: &v1beta1.ObjectType{
+					Namespace: req.GetSubject().GetSubject().GetType().GetNamespace(),
+					Name:      req.GetSubject().GetSubject().GetType().GetName(),
+				},
+				Id: req.GetSubject().GetSubject().GetId(),
+			},
+		}, *resource); err == nil {
+			return updateResponseFromAuthzRequestV1beta2(resp), nil
+		} else {
+			return nil, err
+		}
+	} else {
+		return nil, err
+	}
+}
+
 func authzFromRequest(identity *authnapi.Identity, resource *pb.ObjectReference) (*model.ReporterResourceId, error) {
+	return &model.ReporterResourceId{
+		LocalResourceId: resource.Id,
+		ResourceType:    resource.Type.Name,
+		ReporterId:      identity.Principal,
+		ReporterType:    identity.Type,
+	}, nil
+}
+
+func authzFromRequestV1beta2(identity *authnapi.Identity, resource *pbv1beta2.ObjectReference) (*model.ReporterResourceId, error) {
 	return &model.ReporterResourceId{
 		LocalResourceId: resource.Id,
 		ResourceType:    resource.Type.Name,
@@ -97,10 +170,26 @@ func viewResponseFromAuthzRequest(allowed bool) *pb.CheckResponse {
 	}
 }
 
+func viewResponseFromAuthzRequestV1beta2(allowed bool) *pbv1beta2.CheckResponse {
+	if allowed {
+		return &pbv1beta2.CheckResponse{Allowed: pbv1beta2.CheckResponse_ALLOWED_TRUE}
+	} else {
+		return &pbv1beta2.CheckResponse{Allowed: pbv1beta2.CheckResponse_ALLOWED_FALSE}
+	}
+}
+
 func updateResponseFromAuthzRequest(allowed bool) *pb.CheckForUpdateResponse {
 	if allowed {
 		return &pb.CheckForUpdateResponse{Allowed: pb.CheckForUpdateResponse_ALLOWED_TRUE}
 	} else {
 		return &pb.CheckForUpdateResponse{Allowed: pb.CheckForUpdateResponse_ALLOWED_FALSE}
+	}
+}
+
+func updateResponseFromAuthzRequestV1beta2(allowed bool) *pbv1beta2.CheckForUpdateResponse {
+	if allowed {
+		return &pbv1beta2.CheckForUpdateResponse{Allowed: pbv1beta2.CheckForUpdateResponse_ALLOWED_TRUE}
+	} else {
+		return &pbv1beta2.CheckForUpdateResponse{Allowed: pbv1beta2.CheckForUpdateResponse_ALLOWED_FALSE}
 	}
 }

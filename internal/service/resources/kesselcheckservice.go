044c8f313ee2f61f5ddb32de056323f6b312de88
Sneha Gunta <sgunta@redhat.com>
2025-04-28 16:12:28 -0400

    remove optional that was added by mistake

--- internal/service/resources/kesselcheckservice.go
+++ internal/service/resources/kesselcheckservice.go
@@ -172,9 +172,9 @@ func viewResponseFromAuthzRequest(allowed bool) *pb.CheckResponse {
 
 func viewResponseFromAuthzRequestV1beta2(allowed bool) *pbv1beta2.CheckResponse {
 	if allowed {
-		return &pbv1beta2.CheckResponse{Allowed: AllowedPtr(pbv1beta2.Allowed_ALLOWED_TRUE)}
+		return &pbv1beta2.CheckResponse{Allowed: ToPtr(pbv1beta2.Allowed_ALLOWED_TRUE)}
 	} else {
-		return &pbv1beta2.CheckResponse{Allowed: AllowedPtr(pbv1beta2.Allowed_ALLOWED_FALSE)}
+		return &pbv1beta2.CheckResponse{Allowed: ToPtr(pbv1beta2.Allowed_ALLOWED_FALSE)}
 	}
 }
 
@@ -188,12 +188,12 @@ func updateResponseFromAuthzRequest(allowed bool) *pb.CheckForUpdateResponse {
 
 func updateResponseFromAuthzRequestV1beta2(allowed bool) *pbv1beta2.CheckForUpdateResponse {
 	if allowed {
-		return &pbv1beta2.CheckForUpdateResponse{Allowed: AllowedPtr(pbv1beta2.Allowed_ALLOWED_TRUE)}
+		return &pbv1beta2.CheckForUpdateResponse{Allowed: ToPtr(pbv1beta2.Allowed_ALLOWED_TRUE)}
 	} else {
-		return &pbv1beta2.CheckForUpdateResponse{Allowed: AllowedPtr(pbv1beta2.Allowed_ALLOWED_FALSE)}
+		return &pbv1beta2.CheckForUpdateResponse{Allowed: ToPtr(pbv1beta2.Allowed_ALLOWED_FALSE)}
 	}
 }
 
-func AllowedPtr(a pbv1beta2.Allowed) *pbv1beta2.Allowed {
+func ToPtr(a pbv1beta2.Allowed) *pbv1beta2.Allowed {
 	return &a
 }

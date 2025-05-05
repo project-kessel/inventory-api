930fa3f827cdbc2030f820edb3b14b83b7a929f9
Jonathan Marcantonio <jmarcant@redhat.com>
2025-05-05 15:26:42 -0400

    RHCLOUD-39796 - Add script/runbook for manually replicating missing objects (#542)
    * script seems to work
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Execute delete operations
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Add some operation extraction & start split into cases
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Add helper for fetching data with gabi
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Exit cases depending on response from inv db
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * fix bug in deleted case
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Some cleanup
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Update for cases for missing operation messages
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Update runbook with instructions for gabi setup
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Update docs/runbooks/stage/replicate-inventory-messages-spicedb.md
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update docs/runbooks/stage/replicate-inventory-messages-spicedb.md
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update docs/runbooks/stage/replicate-inventory-messages-spicedb.md
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Split cases into helpers
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * some formatting
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Update script to treat inventory db as source of truth + runbook updates
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * Check that all required inventory fields exist
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * check both v1beta1 & v1beta2 for namespace
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    * cleanup & error checking
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    
    ---------
    
    Signed-off-by: Jonathan Marcantonio <jmarcant@redhat.com>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

---
+++ docs/runbooks/stage/replicate-inventory-messages-spicedb.md
@@ -0,0 +1,139 @@
+# Manual Inventory DB replication to SpiceDB
+
+## Prerequisites
+
+Remediations covered in this guide will require the following:
+1) [zed cli](https://github.com/authzed/zed?tab=readme-ov-file#getting-started)
+2) [gabi cli](https://github.com/app-sre/gabi-cli) installed & gabi access on cluster
+3) Some python3 version installed.
+4) Access to the portforwarding into the running cluster. 
+
+### Configuring Zed CLI
+
+To install the `zed` CLI, refer to [Authzed's README](https://github.com/authzed/zed?tab=readme-ov-file#getting-started) for specific instructions based on your OS
+
+In order to access SpiceDB using the `zed` cli, you'll need to configure a context for the SpiceDB endpoint. This will require a preshared token available in a Kubernetes secret on cluster.
+
+To configure the context:
+
+```shell
+# capture the preshared key from spicedb-config secret
+PSTK=$(oc get secret spicedb-config -o jsonpath='{.data.preshared_key}' | base64 -d)
+
+# create the context with zed cli -- change ENV to the ENV you are targeting (stage, prod)
+zed context set <ENV>-spicedb localhost:50051 $PSTK --insecure
+
+# verify the context was created and is the current context
+zed context list
+```
+
+To confirm the context is working:
+
+```shell
+# port-forward the spicedb service to your system
+oc port-forward svc/kessel-relations-spicedb 50051:50051
+
+# check the schema
+zed schema read
+```
+
+The read command should dump out the entire SpiceDB schema. An error is provided if there are any connection issues or if no schema exists.
+
+### Configuring gabi CLI
+
+To install the `gabi` CLI, refer to [gabi's README](https://github.com/app-sre/gabi-cli) for specific instructions based on your OS
+
+In order to access Inventory's DB using the `gabi` cli, you'll need to configure a profile for the stage gabi instance. Accessing the Gabi instance requires its route and a token used to log into the cluster.
+
+If you don't already have a gabi config initialized, execute the `init` command: 
+```shell
+gabi config init
+```
+There are two settings we need to set up, the `URL` and `TOKEN`. You can grab the `URL` from
+the `gabi-kessel` route and the `TOKEN` from the token used to log into the cluster.
+Execute the following to set your url and token.
+```shell
+gabi config seturl <gabi-kessel-route>
+gabi config settoken <sha256-login-token> 
+```
+You can check your current profile with `gabi config currentprofile`.
+```shell
+{
+  "name": "default",
+  "alias": "default",
+  "url": "https://gabi-kessel.apps.cluster.example.com",
+  "token": "sha256~xkXXX...",
+  "current": true,
+  "enable_history": true
+}
+```
+
+To confirm `gabi` is working we can execute a basic query against the DB.
+```shell
+gabi exec "select id, created_at, reporter from resources limit 1"
+[
+  {
+    "created_at": "2024-11-13T14:29:57.061157Z",
+    "id": "X-X-X-X",
+    "reporter": "{}"
+  }
+]
+```
+
+## Running the migration script.
+
+We will need a dump of the offset messages that need to be reprocessed. To get a dump of processed messages,
+we can spin up a kafka debug pod to read every message.
+
+**Process**
+
+```shell
+# capture Kafka connection details for Kafka CLI commands later
+BOOTSTRAP_SERVERS=$(oc get secret kessel-inventory -o json | jq -r '.data."cdappconfig.json"' | base64 -d | jq -r '.kafka.brokers[] | "\(.hostname):\(.port)"')
+KAFKA_USER=$(oc get secret kessel-inventory -o json | jq -r '.data."cdappconfig.json"' | base64 -d | jq -r '.kafka.brokers[0].sasl.username')
+KAFKA_PW=$(oc get secret kessel-inventory -o json | jq -r '.data."cdappconfig.json"' | base64 -d | jq -r '.kafka.brokers[0].sasl.password')
+
+# run and exec into kafka pod
+oc run kafka-debug --rm -i --tty --image quay.io/strimzi/kafka:0.45.0-kafka-3.9.0 --env BOOTSTRAP_SERVERS="$BOOTSTRAP_SERVERS" --env KAFKA_USER="$KAFKA_USER" --env KAFKA_PW="$KAFKA_PW" -- bash
+
+# setup the config file with credentials information
+cat <<EOF > /tmp/config.props
+sasl.mechanism=SCRAM-SHA-512
+security.protocol=SASL_SSL
+sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required username="$KAFKA_USER" password="$KAFKA_PW";
+EOF
+
+# confirm current offset and lag
+./bin/kafka-consumer-groups.sh --bootstrap-server $BOOTSTRAP_SERVERS --command-config /tmp/config.props --group inventory-consumer --describe
+
+# print all processed messages
+./bin/kafka-console-consumer.sh --topic outbox.event.kessel.tuples --from-beginning  --consumer.config /tmp/config.props --bootstrap-server $BOOTSTRAP_SERVERS --property print.offset=true --property print.key=true --property print.headers=true
+```
+
+Save these messages to some file like `kafkadump.txt`.
+
+**WARNING:**
+Before executing our script we must ensure the consumer is disabled such that no messages are processed at the same time
+we are trying to manually replicate resources to SpiceDB. If you do not disable the consumer before running the migration script, data between inventory and SpiceDB may not be consistent. 
+
+Disable the consumer in the inventory api config yaml. You may need to roll the pods for the change to take effect.
+```shell
+  consumer:
+    enabled: false
+```
+
+With our zed context and gabi config set up to stage and port-forwarding into the SpiceDB service, we can execute our [migration script](/scripts/manual_migration_to_spicedb.py). Move the migration script to the same location as your `kafkadump.txt` file.
+
+Run 
+```shell
+python3 manual_migration_to_spicedb.py kafkadump.txt
+```
+
+Voila! Your inventory DB data should now be consistent with SpiceDB after reprocessing the failing messages!
+
+You can now reenable the consumer so messages can begin processing. You may need to roll the pods for the change to take effect.
+
+```shell
+  consumer:
+    enabled: true
+```
---
+++ scripts/manual_migration_to_spicedb.py
@@ -0,0 +1,211 @@
+import json
+import re
+import subprocess
+import sys
+import os
+
+
+def extract_json_payload(line: str):
+    if "operation:" in line:
+        parts = line.strip().split('\t')
+    
+        # extract the operation
+        op_match = re.search(r'operation:(\w+)', parts[1])
+        if not op_match:
+            return None, None, None
+        operation = op_match.group(1)
+
+        # extract inventory_id from payload
+        payload1_json = json.loads(parts[2])
+        inventory_id = payload1_json.get("payload")
+
+        # get the other payload with the permission data.
+        data = json.loads(parts[-1])
+
+        return inventory_id, data.get("payload"), operation
+    else:
+        match = re.search(r'"payload":"({.+})"', line)
+        if not match:
+            return None, None, None
+        payload_str = match.group(1).encode().decode("unicode_escape")
+
+        # extract inventory_id from payload.
+        match_inventory_id = re.search(r'"payload":"(.{36})"', line)
+        if not match_inventory_id:
+            return None, None, None
+        inventory_id = match_inventory_id.group(1).encode().decode("unicode_escape")
+
+        # We don't really know what operation they are supposed to be doing tbh. 
+        # Update might be safe, but not entirely.
+        return inventory_id, json.loads(payload_str), "updated"
+    
+
+def build_deleted_command(inventory_id, payload):
+    # If the resource does not exist in inventory, check if tuple exists, if so delete the tuple
+	# (parse message, gabi call, zed relationship read, zed relationship delete) 
+
+    # parse message
+    relation = payload["relation"]
+
+    # Call gabi to fetch current info on resource.
+    inv_res_id, inv_sub_id, inv_res_type, inv_report_type = fetch_inventory_resource_info(inventory_id)
+    if not all([inv_res_id, inv_sub_id, inv_res_type, inv_report_type]):
+        return None # Dont delete, as it still exists in inventory DB.
+
+    # zed relationship read fully consistent (drift check)
+    res = subprocess.run(
+        f"zed relationship read {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} --consistency-full",
+        shell=True, capture_output=True, text=True
+    )
+    if len(res.stdout) != 0:
+        print(f"Resource exists in SpiceDB: {res.stdout}. Deleting...")
+        return f"zed relationship bulk-delete {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation}"
+
+    print("Resource doesn't exist in SpiceDB. Nothing to delete...")
+    return None
+
+def build_created_command(inventory_id, payload):
+    # If the resource exists in inventory, check if tuple exists, if not create
+    # (parse message, gabi call, zed relationship read, zed relationship create)
+
+    # parse message
+    relation = payload["relation"]
+    subject = payload["subject"]["subject"]
+
+    subject_ns = subject["type"]["namespace"]
+    subject_name = subject["type"]["name"]
+
+    # Call gabi to fetch current info on resource.
+    inv_res_id, inv_sub_id, inv_res_type, inv_report_type = fetch_inventory_resource_info(inventory_id)
+    if not all([inv_res_id, inv_sub_id, inv_res_type, inv_report_type]):
+        return None # No resource in inventory DB.
+
+    # zed relationship read fully consistent (drift check)
+    res = subprocess.run(
+        f"zed relationship read {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} {subject_ns}/{subject_name}:{inv_sub_id} --consistency-full",
+        shell=True, capture_output=True, text=True
+    )
+    if len(res.stdout) != 0:
+        print(f"Resource exists in SpiceDB: {res.stdout}. Not creating.")
+        return None # Our job here is already done.
+    print("Resource exists in inventory, but not in SpiceDB. Creating...")
+    return f"zed relationship create {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} {subject_ns}/{subject_name}:{inv_sub_id}"
+
+def build_updated_command(inventory_id, payload):
+    # if the resource exists in inventory, check if the tuple exists, if they do not match,
+    # apply the update to the tuple derived from the resource
+    # (parse mesasge, gabi call, zed relatonship read, zed relationship touch)
+
+    # parse message
+    relation = payload["relation"]
+    subject = payload["subject"]["subject"]
+
+    subject_ns = subject["type"]["namespace"]
+    subject_name = subject["type"]["name"]
+
+    # Call gabi to fetch current info on resource.
+    inv_res_id, inv_sub_id, inv_res_type, inv_report_type = fetch_inventory_resource_info(inventory_id)
+    if not all([inv_res_id, inv_sub_id, inv_res_type, inv_report_type]):
+        return None # No resource in inventory DB.
+
+    # zed relationship read fully consistent (drift check)
+    res = subprocess.run(
+        f"zed relationship read {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} {subject_ns}/{subject_name}:{inv_sub_id} --consistency-full",
+        shell=True, capture_output=True, text=True
+    )
+    if len(res.stdout) != 0:
+        print(f"Resource exists in SpiceDB: {res.stdout}")
+        sections = res.stdout.split()
+        if len(sections) < 3:
+            print(f"Unexpected output format from zed relationship read: {res.stdout}")
+            return None
+        current_res_id = sections[0].split(":")[1]
+        current_sub_id = sections[2].split(":")[1]
+        if current_res_id == inv_res_id and current_sub_id == inv_sub_id:
+            print("Resources match, no need to update.")
+            return None # They match, no need to update.
+        print("Updating resource...")
+        return f"zed relationship touch {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} {subject_ns}/{subject_name}:{inv_sub_id}"
+
+    print("Resource doesn't exist in SpiceDB but does in inventory, creating...")
+    return f"zed relationship touch {inv_report_type}/{inv_res_type}:{inv_res_id} t_{relation} {subject_ns}/{subject_name}:{inv_sub_id}"
+    
+
+def build_zed_command(inventory_id: str, payload: any, operation: str):
+    print(f"\nBuilding command for Inventory ID: {inventory_id}, operation: {operation}")
+    if operation == "deleted":
+        return build_deleted_command(inventory_id, payload)
+    elif operation == "created":
+        return build_created_command(inventory_id, payload)
+    elif operation == "updated":
+        return build_updated_command(inventory_id, payload)
+    else:
+        print("Unsupported operation")
+        return None
+
+def fetch_inventory_resource_info(inventory_id):
+    # Call gabi to fetch current info on resource.
+    print(f"Fetching info on inventory_id: {inventory_id}")
+    res = subprocess.run(
+        f"gabi exec \"select reporter_resource_id, workspace_id, resource_type, reporter_type, reporter from resources where inventory_id='{inventory_id}'\"",
+        shell=True,
+        capture_output=True,
+        text=True
+    )
+    gabi_output = res.stdout
+    if "your query didn't return any results" in gabi_output: # there's some weird tabbing in output.
+        print("Nothing in inventory db.")
+        return None, None, None, None # Don't update anything.
+
+    # resource exists
+    print(f"Inventory ID: {inventory_id} exists in Inventory DB.")
+    parsed_data = json.loads(gabi_output)
+    if not parsed_data:
+        return None, None, None, None
+    
+    inv_resource_id = parsed_data[0]["reporter_resource_id"]
+    inv_subject_id = parsed_data[0]["workspace_id"]
+    inv_resource_type = parsed_data[0]["resource_type"]
+
+    inv_reporter_type = parsed_data[0]["reporter_type"].lower()
+    parse_reporter = json.loads(parsed_data[0]["reporter"])
+    inv_reporter_reporter_type = parse_reporter["reporter_type"].lower()
+
+    inv_reporter_type = inv_reporter_type or inv_reporter_reporter_type
+
+    print(f"Inventory_resource_id: {inv_resource_id}")
+    print(f"Inventory_subject_id: {inv_subject_id}")
+    print(f"Inventory_resource_type: {inv_resource_type}")
+    print(f"Inventory_reporter_type: {inv_reporter_type}")
+    return inv_resource_id, inv_subject_id, inv_resource_type, inv_reporter_type
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 manual_migration_to_spicedb.py <input_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+
+    if not os.path.isfile(input_file):
+        print(f"File not found: {input_file}")
+        return
+
+    with open(input_file, "r") as f:
+        for line in f:
+            inventory_id, payload, operation = extract_json_payload(line)
+            if not payload:
+                continue
+
+            cmd = build_zed_command(inventory_id, payload, operation)
+            if cmd:
+                print(f"Running: {cmd}")
+                try:
+                    subprocess.run(cmd, shell=True, check=True)
+                except subprocess.CalledProcessError as e:
+                    print(f"Command failed with return code {e.returncode}")
+                    sys.exit(e.returncode)
+
+
+if __name__ == "__main__":
+    main()

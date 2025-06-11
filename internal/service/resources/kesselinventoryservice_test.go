6ea0887e6d754402e65b46a229b52b6894e41f66
Sneha Gunta <sgunta@redhat.com>
2025-06-11 16:29:35 -0400

    add test back, move validation code into tests

---
+++ internal/service/resources/kesselinventoryservice_test.go
@@ -0,0 +1,37 @@
+package resources
+
+import (
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"testing"
+)
+
+func TestIsValidatedRepresentationType(t *testing.T) {
+
+	assert.True(t, IsValidType("hbi"))
+
+	// capatalised type not normalized
+	assert.False(t, IsValidType("HBI"))
+
+	// normalize then validate
+	normalized := normalizeType("HBI")
+	assert.True(t, IsValidType(normalized))
+	// too long
+	assert.False(t, IsValidType("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+	// strange characters
+	assert.False(t, IsValidType("h?!!!"))
+}
+
+func TestNormalizeRepresentationType(t *testing.T) {
+	// normalize then validate
+	normalized := normalizeType("HBI")
+	assert.True(t, IsValidType(normalized))
+
+	assert.Equal(t, "hbi", normalized)
+}
+
+var typePattern = regexp.MustCompile(`^([a-z][a-z0-9_]{1,61}[a-z0-9]/)*[a-z][a-z0-9_]{1,62}[a-z0-9]$`)
+
+func IsValidType(val string) bool {
+	return typePattern.MatchString(val)
+}

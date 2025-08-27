47bf02ee5e1764f1f84be7a1851907034f6c7cf3
Adam0Brien <aobrien@redhat.com>
2025-08-27 10:32:22 +0100

    add more test cases to reporterReference

--- api/kessel/inventory/v1beta2/reporter_reference.pb_test.go
+++ api/kessel/inventory/v1beta2/reporter_reference.pb_test.go
@@ -3,8 +3,10 @@ package v1beta2_test
 import (
 	"testing"
 
+	"buf.build/go/protovalidate"
 	"github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReporterReference_BasicFields(t *testing.T) {
@@ -25,6 +27,86 @@ func TestReporterReference_NilInstanceId(t *testing.T) {
 	assert.Equal(t, "hbi", r.GetType())
 	assert.Equal(t, "", r.GetInstanceId())
 }
+
+func TestReporterReference_ValidInstanceIdHostname(t *testing.T) {
+	instID := "redhat.com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "redhat.com", r.GetInstanceId())
+}
+
+func TestReporterReference_ValidInstanceIdHostname_NoDot(t *testing.T) {
+	instID := "redhat"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "redhat", r.GetInstanceId())
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_Comma(t *testing.T) {
+	instID := "redhat,com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_Hashtag(t *testing.T) {
+	instID := "redhat#com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_AtSign(t *testing.T) {
+	instID := "redhat@com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdUUID_SingleDigit(t *testing.T) {
+	instID := "3"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+func TestReporterReference_InvalidInstanceIdUUID_MultipleDigits(t *testing.T) {
+	instID := "3-3-3-3-3"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
 
 func TestReporterReference_ResetAndFields(t *testing.T) {
 	instID := "3c4e2382-26c1-11f0-8e5c-ce0194e9e144"

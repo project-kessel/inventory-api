f19f25e6974c9377e79e2531f6c5cab2caeb621a
Adam O'Brien <aobrien@redhat.com>
2025-08-27 14:50:45 +0100

    v1beta2: remove strict regex from reporter reference instance_id (#816)
    * v1beta2: remove strict regex from instance_id
    
    * add more test cases to reporterReference
    
    * add new regex + tests

--- api/kessel/inventory/v1beta2/reporter_reference.pb_test.go
+++ api/kessel/inventory/v1beta2/reporter_reference.pb_test.go
@@ -3,38 +3,144 @@ package v1beta2_test
 import (
 	"testing"
 
+	"buf.build/go/protovalidate"
 	"github.com/project-kessel/inventory-api/api/kessel/inventory/v1beta2"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReporterReference_BasicFields(t *testing.T) {
-	instID := "hbi"
+	instID := "3c4e2382-26c1-11f0-8e5c-ce0194e9e144"
 	r := &v1beta2.ReporterReference{
-		Type:       "host",
+		Type:       "hbi",
 		InstanceId: &instID,
 	}
 
-	assert.Equal(t, "host", r.GetType())
-	assert.Equal(t, "hbi", r.GetInstanceId())
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "3c4e2382-26c1-11f0-8e5c-ce0194e9e144", r.GetInstanceId())
 }
 
 func TestReporterReference_NilInstanceId(t *testing.T) {
 	r := &v1beta2.ReporterReference{
-		Type: "k8s_policy",
+		Type: "hbi",
 	}
-	assert.Equal(t, "k8s_policy", r.GetType())
+	assert.Equal(t, "hbi", r.GetType())
 	assert.Equal(t, "", r.GetInstanceId())
 }
 
-func TestReporterReference_ResetAndFields(t *testing.T) {
-	instID := "hello"
+func TestReporterReference_ValidInstanceIdHostname(t *testing.T) {
+	instID := "redhat.com"
 	r := &v1beta2.ReporterReference{
-		Type:       "test",
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "redhat.com", r.GetInstanceId())
+}
+
+func TestReporterReference_ValidInstanceIdHostname_NoDot(t *testing.T) {
+	instID := "redhat"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "redhat", r.GetInstanceId())
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_Comma(t *testing.T) {
+	instID := "redhat,com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_Hashtag(t *testing.T) {
+	instID := "redhat#com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdHostname_AtSign(t *testing.T) {
+	instID := "redhat@com"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdUUID_SingleDigit(t *testing.T) {
+	instID := "3"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdUUID_MultipleDigits(t *testing.T) {
+	instID := "3-3-3-3-3"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdUUID_TooLong(t *testing.T) {
+	instID := "3c4e2382-26c1-11f0-8e5c-ce0194e9e144-11212"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_InvalidInstanceIdUUID_TooShort(t *testing.T) {
+	instID := "3c4e2382-26c1-11f0-8e5c-ce01944"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
+		InstanceId: &instID,
+	}
+	validator, err := protovalidate.New()
+	require.NoError(t, err)
+	err = validator.Validate(r)
+	assert.Error(t, err)
+}
+
+func TestReporterReference_ResetAndFields(t *testing.T) {
+	instID := "3c4e2382-26c1-11f0-8e5c-ce0194e9e144"
+	r := &v1beta2.ReporterReference{
+		Type:       "hbi",
 		InstanceId: &instID,
 	}
 
-	assert.Equal(t, "test", r.GetType())
-	assert.Equal(t, "hello", r.GetInstanceId())
+	assert.Equal(t, "hbi", r.GetType())
+	assert.Equal(t, "3c4e2382-26c1-11f0-8e5c-ce0194e9e144", r.GetInstanceId())
 
 	r.Reset()
 	assert.Equal(t, "", r.Type)
@@ -42,15 +148,15 @@ func TestReporterReference_ResetAndFields(t *testing.T) {
 }
 
 func TestReporterReference_Methods(t *testing.T) {
-	instID := "xyz"
+	instID := "3c4e2382-26c1-11f0-8e5c-ce0194e9e144"
 	r := &v1beta2.ReporterReference{
-		Type:       "reporter",
+		Type:       "hbi",
 		InstanceId: &instID,
 	}
 
 	// String method
 	s := r.String()
-	assert.Contains(t, s, "reporter")
+	assert.Contains(t, s, "hbi")
 
 	// ProtoMessage, ProtoReflect, Descriptor: just call them for coverage
 	r.ProtoMessage()

caad312491c31d4fa600e0c8cb158276cff44b10
Adam0Brien <aobrien@redhat.com>
2025-10-30 12:27:58 +0000

    return created_at and updated_at values from FindResourceByKeys

--- internal/data/resource_repository.go
+++ internal/data/resource_repository.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/uuid"
@@ -28,6 +29,8 @@ type FindResourceByKeysResult struct {
 	ReporterType          string    `gorm:"column:reporter_type"`
 	ReporterInstanceID    string    `gorm:"column:reporter_instance_id"`
 	ConsistencyToken      string    `gorm:"column:consistency_token"`
+	CreatedAt             time.Time `gorm:"column:created_at"`
+	UpdatedAt             time.Time `gorm:"column:updated_at"`
 }
 
 type RepresentationsByVersion struct {
@@ -85,6 +88,8 @@ func (result FindResourceByKeysResult) ToSnapshots() (bizmodel.ResourceSnapshot,
 		Type:             result.ResourceType,
 		CommonVersion:    result.CommonVersion,
 		ConsistencyToken: result.ConsistencyToken,
+		CreatedAt:        result.CreatedAt,
+		UpdatedAt:        result.UpdatedAt,
 	}
 
 	// Create ReporterResourceKeySnapshot
@@ -261,6 +266,8 @@ func (r *resourceRepository) FindResourceByKeys(tx *gorm.DB, key bizmodel.Report
 		res.common_version,
 		res.id AS resource_id,
 		res.ktn AS consistency_token,
+		res.created_at,
+		res.updated_at,
 		rr2.resource_type,
 		rr2.local_resource_id,
 		rr2.reporter_type,

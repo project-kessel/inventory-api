0f2f300f2e5e7e3b9075447fb40cc2621b391849
Antony Natale <anatale@redhat.com>
2026-02-17 14:22:51 -0500

    removes postgres17 to avoid konflux issues, adds checksum checks and curl improvements

--- tools/kessel-debug-container/installer.sh
+++ tools/kessel-debug-container/installer.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
 
 echo "Setting up pre-reqs..."
 microdnf install -y jq
 
 echo "Fetching required packages and RPM files..."
-GRPCURL_VERSION=$(curl https://api.github.com/repos/fullstorydev/grpcurl/releases/latest  | jq -r '.tag_name')
-ZED_VERSION=$(curl https://api.github.com/repos/authzed/zed/releases/latest | jq -r '.tag_name')
+GRPCURL_VERSION=$(curl -sSfL https://api.github.com/repos/fullstorydev/grpcurl/releases/latest | jq -er '.tag_name')
+ZED_VERSION=$(curl -sSfL https://api.github.com/repos/authzed/zed/releases/latest | jq -er '.tag_name')
 # the tags fetched for grpcurl and zed will be in the format of 'vX.Y.Z', so bash substrings are used below to strip the 'v'
-curl -Lo /tmp/grpcurl.rpm https://github.com/fullstorydev/grpcurl/releases/download/v${GRPCURL_VERSION:1}/grpcurl_${GRPCURL_VERSION:1}_linux_amd64.rpm && \
-curl -Lo /tmp/zed.rpm https://github.com/authzed/zed/releases/download/v${ZED_VERSION:1}/zed_${ZED_VERSION:1}_linux_amd64.rpm
-curl -Lo /tmp/kafka.tgz https://dlcdn.apache.org/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
+GRPCURL_VERSION=${GRPCURL_VERSION#v}
+ZED_VERSION=${ZED_VERSION#v}
 
+# Download grpcurl and zed RPM and checksums
+curl -fLo /tmp/grpcurl_${GRPCURL_VERSION}_linux_amd64.rpm https://github.com/fullstorydev/grpcurl/releases/download/v${GRPCURL_VERSION}/grpcurl_${GRPCURL_VERSION}_linux_amd64.rpm
+curl -fLo /tmp/grpcurl_checksums.txt https://github.com/fullstorydev/grpcurl/releases/download/v${GRPCURL_VERSION}/grpcurl_${GRPCURL_VERSION}_checksums.txt
+curl -fLo /tmp/zed_${ZED_VERSION}_linux_amd64.rpm https://github.com/authzed/zed/releases/download/v${ZED_VERSION}/zed_${ZED_VERSION}_linux_amd64.rpm
+curl -fLo /tmp/zed_checksums.txt https://github.com/authzed/zed/releases/download/v${ZED_VERSION}/checksums.txt
+
+# Verify grpcurl and zed checksums
+pushd /tmp
+grep "grpcurl_${GRPCURL_VERSION}_linux_amd64.rpm" grpcurl_checksums.txt | sha256sum -c -
+grep "zed_${ZED_VERSION}_linux_amd64.rpm" zed_checksums.txt | sha256sum -c -
+popd
+
+# Download kafka tools
+curl -fLo /tmp/kafka.tgz https://dlcdn.apache.org/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
 
 echo "Setting up EPEL Repository..."
 rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
-echo "Setting up Postgresql 17 Repository..."
-rpm -ivh https://download.postgresql.org/pub/repos/yum/reporpms/EL-9-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-
 echo "Installing packages..."
-microdnf install -y tar gzip wget bind-utils nmap-ncat openssl vim java-21-openjdk kcat postgresql17 util-linux less
-rpm -ivh /tmp/grpcurl.rpm /tmp/zed.rpm
+microdnf install -y tar gzip wget bind-utils nmap-ncat openssl vim java-21-openjdk kcat postgresql util-linux less
+rpm -ivh /tmp/grpcurl_${GRPCURL_VERSION}_linux_amd64.rpm /tmp/zed_${ZED_VERSION}_linux_amd64.rpm
 
 mkdir -pv /opt/kafka
 tar -xvf /tmp/kafka.tgz -C /opt/kafka --strip-components=1
@@ -34,3 +44,4 @@ mv /usr/local/bin/zed-wrapper.sh /usr/local/bin/zed
 echo "Clean up..."
 microdnf clean all -y
 rm /tmp/*.rpm
+rm /tmp/*.txt

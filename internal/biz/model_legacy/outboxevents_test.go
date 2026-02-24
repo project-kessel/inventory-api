42bc6fc064fee069c3c12a99ce81f6c214aaf3fd
Rajagopalan Ranganathan <rarangan@redhat.com>
2026-02-24 15:18:41 +0000

    more coerabbit comments fixed

--- internal/biz/model_legacy/outboxevents_test.go
+++ internal/biz/model_legacy/outboxevents_test.go
@@ -155,7 +155,7 @@ func assertResourceEventFromDomainEvent(t *testing.T, operation bizmodel.EventOp
 }
 
 // TestEventResourceReporter_ReporterVersion_Optional documents that EventResourceReporter
-// uses *string for ReporterVersion.
+// uses *string for ReporterVersion (nil = not set).
 func TestEventResourceReporter_ReporterVersion_Optional(t *testing.T) {
 	// Nil ReporterVersion round-trips as null in JSON (omitempty omits the field when nil)
 	er := EventResourceReporter{
